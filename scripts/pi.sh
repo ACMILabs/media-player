40ca607d80b44159d87dc1d818b6acef3ae7d2c9
Simon Loffler <simon.loffler@acmi.net.au>
2019-07-18 17:35:12 +1000

    Install X and set VLC to run under root

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Allow VLC to run under root
+sed -i 's/geteuid/getppid/' /usr/bin/vlc
+
+# Start X
+rm /tmp/.X0-lock &>/dev/null || true
+echo "Starting X in 2 seconds"
+sleep 2
+startx &
+sleep 2
+
 python media_player.py
