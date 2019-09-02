7434e892e14bc23cebe5de7f66fd2a7b4e9173c5
Simon Loffler <simon.loffler@acmi.net.au>
2019-09-02 15:54:02 +1000

    Try DISPLAY=:0

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -3,14 +3,9 @@
 # Allow VLC to run under root
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 
-export DISPLAY=:0.0
+# export DISPLAY=:0.0
+export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-# rotate screen if env variable is set [normal, inverted, left or right]
-if [[ ! -z "$ROTATE_DISPLAY" ]]; then
-  echo "YES"
-  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) & 
-fi
 
 # start desktop manager
 echo "STARTING X"
