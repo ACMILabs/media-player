4661b3aa867f69cc89adad4ff3a39746b97d4fb0
Simon Loffler <simon.loffler@acmi.net.au>
2019-09-30 13:12:00 +1000

    #19 Remove commented code.

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -15,27 +15,8 @@ startx &
 # TODO: work out how to detect X has started
 sleep 5
 
-# uncomment to start x without mouse cursor
-# startx -- -nocursor &
-
-# uncomment to open an application instead of the desktop
-# startx xterm 
-
 # Hide the cursor
 unclutter -display :0 -idle 0.1 &
-
-# Set background image
-# export DISPLAY=:0
-# pcmanfm --set-wallpaper /code/resources/acmi-1920x1080.png
-
-# Set X background image
-# xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /code/resources/acmi-1920x1080.png
-
-# Hide X icons
-# xfconf-query -c xfce4-desktop -np '/desktop-icons/style' -t 'int' -s '0'
-
-# Hide X panel
-# xfce4-panel -q
 
 # Unmute system audio
 # ./scripts/unmute.sh
