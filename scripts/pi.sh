245e53093fc56cfaafe77a4bc6eeb663b33c043a
Simon Loffler <simon.loffler@acmi.net.au>
2019-09-30 14:59:53 +1000

    #19 Add comments to the pi start script.

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -3,9 +3,13 @@
 # Allow VLC to run under root
 sed -i 's/geteuid/getppid/' /usr/bin/vlc
 
+# Remove the X server lock file so ours starts cleanly
 rm /tmp/.X0-lock &>/dev/null || true
 
+# Set the display to use
 export DISPLAY=:0
+
+# Set the DBUS address for sending around system messages
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # start desktop manager
