0057add267a04b3eb40184942d99658eeadb0d59
Simon Loffler <simon.loffler@acmi.net.au>
2019-09-30 10:31:20 +1000

    #19 Try XFCE4 for desktop control.

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -22,16 +22,16 @@ sleep 5
 # startx xterm 
 
 # Hide the cursor
-# unclutter -display :0 -idle 0.1 &
+unclutter -display :0 -idle 0.1 &
 
 # Set X background image
-# xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /code/resources/acmi-1920x1080.png
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /code/resources/acmi-1920x1080.png
 
 # Hide X icons
-# xfconf-query -c xfce4-desktop -np '/desktop-icons/style' -t 'int' -s '0'
+xfconf-query -c xfce4-desktop -np '/desktop-icons/style' -t 'int' -s '0'
 
 # Hide X panel
-# xfce4-panel -q
+xfce4-panel -q
 
 # Unmute system audio
 # ./scripts/unmute.sh
