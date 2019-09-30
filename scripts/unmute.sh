af2f7173c69e7ac1307d96091d617ad7b133f3fb
Simon Loffler <simon.loffler@acmi.net.au>
2019-09-30 16:08:29 +1000

    #19 Add Raspberry Pi mute/unmute commands

--- scripts/unmute.sh
+++ scripts/unmute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Mute system audio
+# Unmute system audio on Intel devices
 amixer -c 0 set Master playback 100% unmute
 
+# Unmute system audio on Raspberry Pis
+amixer set PCM unmute
