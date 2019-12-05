e79f9f04ea1fdbc30727445c4302efb48f379834
David Amores <davidamores3@gmail.com>
2019-12-05 15:19:29 +1100

    #1297 Lint tests

--- tests/tests.py
+++ tests/tests.py
@@ -56,8 +56,8 @@ def test_media_player():
     assert all([vlc_var is not None for vlc_var in media_player.vlc.values()])
 
 
-@patch('requests.get', side_effect=mocked_requests_get)
-def test_download_playlist_from_xos(mock_get):
+@patch('requests.get', MagicMock(side_effect=mocked_requests_get))
+def test_download_playlist_from_xos():
     """
     Test download_playlist_from_xos() returns a valid playlist.
     """
@@ -71,20 +71,24 @@ def test_download_playlist_from_xos(mock_get):
     assert playlist[0]['subtitles'] == '/data/resources/sample.srt'
 
 
-@patch('os.remove')
-@patch('requests.get', side_effect=mocked_requests_get)
-def test_delete_unneeded_resources(mock_get, os_remove):
+@patch('os.remove', MagicMock())
+@patch('requests.get', MagicMock(side_effect=mocked_requests_get))
+def test_delete_unneeded_resources():
     """
     Test delete_unneeded_resources() deletes the expected files.
     """
 
     media_player = MediaPlayer()
-    playlist = json.loads(file_to_string_strip_new_lines('data/playlist.json'))['playlist_labels']
+    playlist = json.loads(
+        file_to_string_strip_new_lines('data/playlist.json')
+    )['playlist_labels']
     files_deleted = media_player.delete_unneeded_resources(playlist)
 
     assert len(files_deleted) == 0
 
-    playlist_2 = json.loads(file_to_string_strip_new_lines('data/playlist-2.json'))['playlist_labels']
+    playlist_2 = json.loads(
+        file_to_string_strip_new_lines('data/playlist-2.json')
+    )['playlist_labels']
     files_deleted_2 = media_player.delete_unneeded_resources(playlist_2)
 
     assert len(files_deleted_2) == 2
@@ -92,9 +96,9 @@ def test_delete_unneeded_resources(mock_get, os_remove):
     assert 'sample.srt' in files_deleted_2
 
 
-@patch('alsaaudio.Mixer')
-@patch('alsaaudio.mixers')
-def test_get_media_player_status(mixer, mixers):
+@patch('alsaaudio.Mixer', MagicMock())
+@patch('alsaaudio.mixers', MagicMock())
+def test_get_media_player_status():
     """
     Test get_media_player_status correctly outputs playback data.
     """
