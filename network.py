4270ac170f66806675c826c8be2fa368132a3767
Simon Loffler <simon.loffler@acmi.net.au>
2021-05-20 11:48:02 +0930

    #182 Shutdown, close, and then set self.sock again after an OSError receiving server messages in the client

--- network.py
+++ network.py
@@ -88,7 +88,9 @@ class Client:  # pylint: disable=R0903
             return None
         except OSError:
             print(f'Closing socket: {self.sock}')
+            self.sock.shutdown()
             self.sock.close()
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print('Attempting to reconnect...')
             self.connect()
             return None
